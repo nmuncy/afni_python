6e3e26bf51ccd74b92dd36cf16cf1aac80612702
Nathan Muncy <nmuncy@fiu.edu>
2020-09-17 09:07:07 -0400

    step1 working

--- step1_submit.sh
+++ step1_submit.sh
@@ -19,13 +19,15 @@ cd $dsetDir
 for i in s*; do
     for j in ${sessList[@]}; do
         for k in ${phaseList[@]}; do
+            if [ ! -f ${workDir}/${i}/${j}/run-1_${k}_scale+tlrc.HEAD ]; then
 
-            sbatch \
-            -o ${outDir}/output_TS1_${i}_${j}_${k}.txt \
-            -e ${outDir}/error_TS1_${i}_${j}_${k}.txt \
-            ${codeDir}/step1_wrap_py.sh $i $j $k $codeDir
+                sbatch \
+                -o ${outDir}/output_TS1_${i}_${j}_${k}.txt \
+                -e ${outDir}/error_TS1_${i}_${j}_${k}.txt \
+                ${codeDir}/step1_wrap_py.sh $i $j $k $codeDir
 
-            sleep 1
+                sleep 1
+            fi
         done
     done
 done
