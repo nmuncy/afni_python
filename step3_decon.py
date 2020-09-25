ebf61841dcf458179beb06174e77af2f7ac12e7e
Nathan Muncy <nmuncy@fiu.edu>
2020-09-25 09:55:40 -0400

    updated, more testing

--- step3_decon.py
+++ step3_decon.py
@@ -1,7 +1,18 @@
 """
 Notes
 
-Still only accepts one phase per session. Update this in the future.
+Timing files should be named "tf_phase_behavior.txt"
+    e.g. tf_vCAT_Hit.txt, or tf_test_FA.txt
+
+If using dmBLOCK option, duration should be married
+    to start time.
+
+GAM, 2GAM base models not using duration - just use
+    BLOCK!
+
+TODO:
+    1) Finish writing/testing for multiple phases
+    2) Test different basis models
 """
 
 # %%
@@ -13,28 +24,7 @@ import fnmatch
 from step1_preproc import func_sbatch
 
 
-def func_pmBlock(tf_dict):
-    h_reg_beh = ""
-    for c_beh, beh in enumerate(tf_dict):
-        h_reg_beh += f'-stim_times_AM1 {c_beh + 1} {tf_dict[beh]} "dmBLOCK(1)" -stim_label {c_beh + 1} {beh} '
-    return h_reg_beh
-
-
-def func_gam(tf_dict):
-    h_reg_beh = ""
-    for c_beh, beh in enumerate(tf_dict):
-        h_reg_beh += f'-stim_times {c_beh + 1} {tf_dict[beh]} "GAM" -stim_label {c_beh + 1} {beh} '
-    return h_reg_beh
-
-
-def func_twoGam(df_dict):
-    h_reg_beh = ""
-    for c_beh, beh in enumerate(tf_dict):
-        h_reg_beh += f'-stim_times {c_beh + 1} {tf_dict[beh]} "TWOGAMpw(4,5,0.2,12,7)" -stim_label {c_beh + 1} {beh} '
-    return h_reg_beh
-
-
-def func_decon(run_files, mot_files, tf_dict, cen_file, h_out, h_type):
+def func_decon(run_files, mot_files, tf_dict, cen_file, h_str, h_type):
 
     in_files = ""
     for fil in run_files:
@@ -44,12 +34,16 @@ def func_decon(run_files, mot_files, tf_dict, cen_file, h_out, h_type):
     for c, mot in enumerate(mot_files):
         reg_base += f"-ortvec {mot} mot_demean_run{c+1} "
 
-    if h_type == "pmBLOCK":
-        reg_beh = func_pmBlock(tf_dict)
-    elif h_type == "GAM":
-        reg_beh = func_gam(tf_dict)
-    elif h_type == "2GAM":
-        reg_beh = func_twoGam(tf_dict)
+    reg_beh = ""
+    for c_beh, beh in enumerate(tf_dict):
+        if h_type == "dmBLOCK":
+            reg_beh += f'-stim_times_AM1 {c_beh + 1} {tf_dict[beh]} "dmBLOCK(1)" -stim_label {c_beh + 1} {beh} '
+        elif h_type == "GAM":
+            reg_beh += f'-stim_times {c_beh + 1} {tf_dict[beh]} "GAM" -stim_label {c_beh + 1} {beh} '
+        elif h_type == "2GAM":
+            reg_beh += f'-stim_times {c_beh + 1} {tf_dict[beh]} "TWOGAMpw(4,5,0.2,12,7)" -stim_label {c_beh + 1} {beh} '
+
+    h_out = f"{h_str}_{h_type}"
 
     cmd_decon = f""" 3dDeconvolve \\
         -x1D_stop \\
@@ -74,6 +68,7 @@ def func_decon(run_files, mot_files, tf_dict, cen_file, h_out, h_type):
 subj = "sub-006"
 sess = "ses-S1"
 phase = "vCAT"
+decon_type = "dmBLOCK"
 
 par_dir = "/scratch/madlab/nate_vCAT"
 data_dir = os.path.join(par_dir, "dset", subj, sess)
@@ -83,6 +78,14 @@ work_dir = os.path.join(par_dir, "derivatives", subj, sess)
 # %%
 """
 Step 1: Make motion regressors
+
+Creates motion, demean, and derivative files. Demeaned
+    are the ones used in the deconvolution.
+
+Censor file is combination of 2 things:
+    1) Censors based on >0.3 rot/translation relative to previous
+        volume. Previous volume censored also.
+    2) Which volumes had >10% outlier voxels.
 """
 run_list = [
     x for x in os.listdir(work_dir) if fnmatch.fnmatch(x, f"*{phase}_scale+tlrc.HEAD")
@@ -95,7 +98,6 @@ h_cmd = f"""
 
     cat dfile.run-*_{phase}.1D > dfile_rall_{phase}.1D
 
-    # files: de-meaned, motion params (per phase)
     1d_tool.py -infile dfile_rall_{phase}.1D \
         -set_nruns {num_run} \
         -demean \
@@ -117,7 +119,6 @@ h_cmd = f"""
         -censor_prev_TR \
         -censor_motion 0.3 motion_{phase}
 
-    # determine censor
     cat out.cen.run-*{phase}.1D > outcount_censor_{phase}.1D
     1deval -a motion_{phase}_censor.1D -b outcount_censor_{phase}.1D \
         -expr "a*b" > censor_{phase}_combined.1D
@@ -129,6 +130,15 @@ if not os.path.exists(os.path.join(work_dir, f"censor_{phase}_combined.1D")):
 # %%
 """
 Step 2: Deconvolve
+
+Uses 3dDeconvolve to generate matrix. 3dREMLfit is then used
+    to do a GLS with an ARMA function.
+
+White matter time series is used as a nuissance regressor.
+
+Deconvolve script written for review.
+
+Base models include pmBLOCK, GAM, and TWOGAMpw.
 """
 
 # Get motion files
@@ -138,9 +148,9 @@ mot_list = [
 mot_list.sort()
 
 
-# Get timing files - all are named *_phase_beh.txt
+# Get timing files - all are named tf_phase_beh.txt
 #   e.g. tf_vCAT_hit.txt
-tf_list = [x for x in os.listdir(work_dir) if fnmatch.fnmatch(x, f"*{phase}*.txt")]
+tf_list = [x for x in os.listdir(work_dir) if fnmatch.fnmatch(x, f"tf*{phase}*.txt")]
 
 # make timing file dictionary
 tf_dict = {}
@@ -150,15 +160,21 @@ for i in tf_list:
     tf_dict[beh] = i
 
 # write decon script, generate matrices and REML_cmd
-decon_script = os.path.join(work_dir, f"decon_{phase}.sh")
+decon_script = os.path.join(work_dir, f"decon_{phase}_{decon_type}.sh")
 with open(decon_script, "w") as script:
     script.write(
         func_decon(
-            run_list, mot_list, tf_dict, f"censor_{phase}_combined.1D", phase, "pmBLOCK"
+            run_list,
+            mot_list,
+            tf_dict,
+            f"censor_{phase}_combined.1D",
+            phase,
+            decon_type,
         )
     )
 
-if not os.path.exists(os.path.join(work_dir, f"X.{phase}.xmat.1D")):
+# run decon script to generate matrices
+if not os.path.exists(os.path.join(work_dir, f"X.{phase}_{decon_type}.xmat.1D")):
     h_cmd = f"cd {work_dir} \n source {decon_script}"
     func_sbatch(h_cmd, 1, 1, 1, "decon", work_dir)
 
@@ -174,8 +190,8 @@ if not os.path.exists(os.path.join(work_dir, f"{phase}_WMe_rall+tlrc.HEAD")):
     func_sbatch(h_cmd, 1, 1, 4, "wmts", work_dir)
 
 # run REML
-if not os.path.exists(os.path.join(work_dir, f"{phase}_stats_REML+tlrc.HEAD")):
-    h_cmd = (
-        f"cd {work_dir} \n tcsh -x {phase}_stats.REML_cmd -dsort {phase}_WMe_rall+tlrc"
-    )
+if not os.path.exists(
+    os.path.join(work_dir, f"{phase}_{decon_type}_stats_REML+tlrc.HEAD")
+):
+    h_cmd = f"cd {work_dir} \n tcsh -x {phase}_{decon_type}_stats.REML_cmd -dsort {phase}_WMe_rall+tlrc"
     func_sbatch(h_cmd, 10, 6, 4, "reml", work_dir)
