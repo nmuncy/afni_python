a173f63d20cd852651007f4167e9ca5aacf841ba
Nathan Muncy <nmuncy@fiu.edu>
2020-09-25 15:26:00 -0400

    found bug, added check

--- step3_submit.sh
+++ step3_submit.sh
@@ -17,12 +17,16 @@ for i in s*; do
     for j in ${sessList[@]}; do
         for k in ${phaseList[@]}; do
 
-            sbatch \
-            -o ${outDir}/output_TS3_${i}_${j}_${k}.txt \
-            -e ${outDir}/error_TS3_${i}_${j}_${k}.txt \
-            ${codeDir}/step1_wrap_py.sh $i $j $k $deconType $workDir $codeDir
+            outFile=${workDir}/${j}/${k}/X.${pahseList[0]}_${deconType}.xmat.1D
+            if [ ! -f $outFile ]; then
 
-            sleep 1
+                sbatch \
+                -o ${outDir}/output_TS3_${i}_${j}_${k}.txt \
+                -e ${outDir}/error_TS3_${i}_${j}_${k}.txt \
+                ${codeDir}/step3_wrap_py.sh $i $j $k $deconType $workDir $codeDir
+
+                sleep 1
+            fi
         done
     done
 done
