687ccdb023ad6905ec6ca23ddcd666883b076642
Nathan Muncy <nmuncy@fiu.edu>
2020-09-29 16:22:18 -0400

    done updating to python wrappers

--- step3_submit.py
+++ step3_submit.py
@@ -33,34 +33,33 @@ os.makedirs(out_dir)
 subj_list = [x for x in os.listdir(deriv_dir) if fnmatch.fnmatch(x, "sub-*")]
 
 # %%
-# for i in subj_list:
-i = "sub-006"
-for j in sess_list:
-    for k in phase_list:
-        if not os.path.exists(
-            os.path.join(
-                deriv_dir,
-                i,
-                j,
-                f"{k}_{decon_type}_stats_REML+tlrc.HEAD",
-            )
-        ):
+for i in subj_list:
+    for j in sess_list:
+        for k in phase_list:
+            if not os.path.exists(
+                os.path.join(
+                    deriv_dir,
+                    i,
+                    j,
+                    f"{k}_{decon_type}_stats_REML+tlrc.HEAD",
+                )
+            ):
 
-            h_out = os.path.join(out_dir, f"out_{i}_{j}_{k}.txt")
-            h_err = os.path.join(out_dir, f"err_{i}_{j}_{k}.txt")
+                h_out = os.path.join(out_dir, f"out_{i}_{j}_{k}.txt")
+                h_err = os.path.join(out_dir, f"err_{i}_{j}_{k}.txt")
 
-            sbatch_job = f"""
-                sbatch \
-                -J "TS3" -t 10:00:00 --mem=4000 --ntasks-per-node=1 \
-                -p centos7_IB_44C_512G  -o {h_out} -e {h_err} \
-                --account iacc_madlab --qos pq_madlab \
-                --wrap="module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
-                python {code_dir}/step3_decon.py {i} {j} {k} {decon_type} {deriv_dir}"
-            """
+                sbatch_job = f"""
+                    sbatch \
+                    -J "TS3" -t 10:00:00 --mem=4000 --ntasks-per-node=1 \
+                    -p centos7_IB_44C_512G  -o {h_out} -e {h_err} \
+                    --account iacc_madlab --qos pq_madlab \
+                    --wrap="module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
+                    python {code_dir}/step3_decon.py {i} {j} {k} {decon_type} {deriv_dir}"
+                """
 
-            sbatch_submit = subprocess.Popen(
-                sbatch_job, shell=True, stdout=subprocess.PIPE
-            )
-            job_id, error = sbatch_submit.communicate()
-            print(job_id)
+                sbatch_submit = subprocess.Popen(
+                    sbatch_job, shell=True, stdout=subprocess.PIPE
+                )
+                job_id, error = sbatch_submit.communicate()
+                print(job_id)
 # %%
