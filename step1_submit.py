5287456922c562afb331b35c778f10847580b9bb
Nathan Muncy <nmuncy@fiu.edu>
2020-10-19 11:36:33 -0400

    added blip toggle, testing ppi full

--- step1_submit.py
+++ step1_submit.py
@@ -20,12 +20,14 @@ TODO:
 import os
 from datetime import datetime
 import subprocess
+import time
 
 # set up
 code_dir = "/home/nmuncy/compute/afni_python"
-work_dir = "/scratch/madlab/nate_vCAT"
-sess_list = ["ses-S1"]
-phase_list = ["vCAT"]
+work_dir = "/scratch/madlab/nate_ppi"
+sess_list = ["ses-S2"]
+phase_list = ["rest"]
+blip_toggle = 0  # 1 = on, 0 = off
 
 # set up stdout/err capture
 current_time = datetime.now()
@@ -56,7 +58,7 @@ for i in subj_list:
                     -p centos7_IB_44C_512G  -o {h_out} -e {h_err} \
                     --account iacc_madlab --qos pq_madlab \
                     --wrap="module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
-                    python {code_dir}/step1_preproc.py {i} {j} {k} {work_dir}"
+                    python {code_dir}/step1_preproc.py {i} {j} {k} {work_dir} {blip_toggle}"
                 """
 
                 sbatch_submit = subprocess.Popen(
@@ -64,4 +66,6 @@ for i in subj_list:
                 )
                 job_id, error = sbatch_submit.communicate()
                 print(job_id)
+
+                time.sleep(1)
 # %%
