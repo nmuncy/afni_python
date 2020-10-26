8a626fb88c509f897d8b2232a5990a78a8c120a2
Nathan Muncy <nmuncy@fiu.edu>
2020-10-26 12:54:26 -0400

    organize MRI working

--- mvpa_step1_setup.py
+++ mvpa_step1_setup.py
@@ -14,6 +14,7 @@ subj_dir = "/scratch/madlab/nate_vCAT/derivatives/sub-005/ses-S1"
 phase = "loc"
 decon_type = "2GAM"
 len_tr = 1.76
+task_dict = {"loc": ["face", "scene", "num"]}
 
 
 # %%
@@ -26,55 +27,57 @@ Should I just train on deconvolved sub-bricks?
 """
 subj_num = subj.split("-")[1]
 
-# get proper brick length
-#   REML appends an extra brick because
-#   "reasons". Account for AFNI's random
-#   0-1 indexing
-h_cmd = f"module load afni-20.2.06 \n 3dinfo -nv {subj_dir}/{phase}_{decon_type}_cbucket_REML+tlrc"
-h_len = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
-len_wrong = h_len.communicate()[0].decode("utf-8").strip()
-len_right = int(len_wrong) - 2
+for phase in task_dict.keys():
 
-# list all scale files
-scale_list = [
-    x.split(".")[0]
-    for x in os.listdir(subj_dir)
-    if fnmatch.fnmatch(x, f"*{phase}*scale+tlrc.HEAD")
-]
-scale_list.sort()
+    # get proper brick length
+    #   REML appends an extra brick because
+    #   "reasons". Account for AFNI's random
+    #   0-1 indexing
+    h_cmd = f"module load afni-20.2.06 \n 3dinfo -nv {subj_dir}/{phase}_{decon_type}_cbucket_REML+tlrc"
+    h_len = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+    len_wrong = h_len.communicate()[0].decode("utf-8").strip()
+    len_right = int(len_wrong) - 2
 
-# make clean data
-if not os.path.exists(os.path.join(subj_dir, f"CleanData_{phase}+tlrc.HEAD")):
+    # list all scale files
+    scale_list = [
+        x.split(".")[0]
+        for x in os.listdir(subj_dir)
+        if fnmatch.fnmatch(x, f"*{phase}*scale+tlrc.HEAD")
+    ]
+    scale_list.sort()
 
-    # list undesirable sub-bricks (those starting with Run or mot)
-    no_int = []
-    with open(os.path.join(subj_dir, f"X.{phase}_{decon_type}.xmat.1D")) as f:
-        h_file = f.readlines()
-        for line in h_file:
-            if line.__contains__("ColumnLabels"):
-                col_list = (
-                    line.replace("#", "").split('"')[1].replace(" ", "").split(";")
-                )
-                for i, j in enumerate(col_list):
-                    if fnmatch.fnmatch(j, "Run*") or fnmatch.fnmatch(j, "mot*"):
-                        no_int.append(f"{str(i)}")
+    # make clean data
+    if not os.path.exists(os.path.join(subj_dir, f"CleanData_{phase}+tlrc.HEAD")):
 
-    # strip extra sub-brick, make clean data by removing
-    #   effects of no interest from concatenated runs
-    h_cmd = f"""
-        cd {subj_dir}
-        3dTcat -prefix tmp_{phase}_cbucket -tr {len_tr} "{phase}_{decon_type}_cbucket_REML+tlrc[0..{len_right}]"
-        3dSynthesize -prefix tmp_effNoInt_{phase} -matrix X.{phase}_{decon_type}.xmat.1D \
-            -cbucket tmp_{phase}_cbucket+tlrc -select {" ".join(no_int)} -cenfill nbhr
-        3dTcat -prefix tmp_all_runs_{phase} -tr {len_tr} {" ".join(scale_list)}
-        3dcalc -a tmp_all_runs_{phase}+tlrc -b tmp_effNoInt_{phase}+tlrc -expr 'a-b' -prefix CleanData_{phase}
-    """
-    func_sbatch(h_cmd, 1, 4, 1, f"{subj_num}cle", subj_dir)
+        # list undesirable sub-bricks (those starting with Run or mot)
+        no_int = []
+        with open(os.path.join(subj_dir, f"X.{phase}_{decon_type}.xmat.1D")) as f:
+            h_file = f.readlines()
+            for line in h_file:
+                if line.__contains__("ColumnLabels"):
+                    col_list = (
+                        line.replace("#", "").split('"')[1].replace(" ", "").split(";")
+                    )
+                    for i, j in enumerate(col_list):
+                        if fnmatch.fnmatch(j, "Run*") or fnmatch.fnmatch(j, "mot*"):
+                            no_int.append(f"{str(i)}")
+
+        # strip extra sub-brick, make clean data by removing
+        #   effects of no interest from concatenated runs
+        h_cmd = f"""
+            cd {subj_dir}
+            3dTcat -prefix tmp_{phase}_cbucket -tr {len_tr} "{phase}_{decon_type}_cbucket_REML+tlrc[0..{len_right}]"
+            3dSynthesize -prefix tmp_effNoInt_{phase} -matrix X.{phase}_{decon_type}.xmat.1D \
+                -cbucket tmp_{phase}_cbucket+tlrc -select {" ".join(no_int)} -cenfill nbhr
+            3dTcat -prefix tmp_all_runs_{phase} -tr {len_tr} {" ".join(scale_list)}
+            3dcalc -a tmp_all_runs_{phase}+tlrc -b tmp_effNoInt_{phase}+tlrc -expr 'a-b' -prefix CleanData_{phase}
+        """
+        func_sbatch(h_cmd, 1, 4, 1, f"{subj_num}cle", subj_dir)
 
 
 # %%
 """
-Step 2:
+Step 2: Organize MRI data
 """
 # pymvpa dirs
 py_dirs = ["BOLD", "anatomy", "model", "masks"]
@@ -104,7 +107,6 @@ for i in atropos_dict:
         out, err = h_mask.communicate()
         print(out, err)
 
-# %%
 # make resampled GM-intersection mask
 if not os.path.exists(os.path.join(mask_dir, "GM_int_mask.nii.gz")):
     h_cmd = f"""
@@ -123,3 +125,37 @@ if not os.path.exists(os.path.join(mask_dir, "GM_int_mask.nii.gz")):
     func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}msk", subj_dir)
 
 # %%
+# BOLD - split into runs
+for count, phase in enumerate(task_dict.keys()):
+
+    h_cmd = (
+        f"module load afni-20.2.06 \n 3dinfo -ntimes {subj_dir}/CleanData_{phase}+tlrc"
+    )
+    h_nvol = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+    num_nvol = int(h_nvol.communicate()[0].decode("utf-8").strip())
+    num_runs = len(scale_list)
+    len_run = int(num_nvol / num_runs)
+
+    beg_vol = 0
+    end_vol = len_run - 1
+    for run in range(1, num_runs + 1):
+        bold_dir = os.path.join(subj_dir, f"BOLD/task00{count+1}_run00{run}")
+        if not os.path.exists(bold_dir):
+            os.makedirs(bold_dir)
+        if not os.path.exists(os.path.join(bold_dir, "bold.nii.gz")):
+            h_cmd = f"""
+                cd {subj_dir}
+                3dTcat -prefix tmp_run-{run}_{phase}_CleanData -tr {len_tr} "CleanData_{phase}+tlrc[{beg_vol}..{end_vol}]"
+                3dcopy tmp_run-{run}_{phase}_CleanData+tlrc {bold_dir}/bold.nii.gz
+            """
+            # print(h_cmd)
+            func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}spl", subj_dir)
+        beg_vol += len_run
+        end_vol += len_run
+
+
+# %%
+"""
+Step 3: Organize timing files
+
+"""
